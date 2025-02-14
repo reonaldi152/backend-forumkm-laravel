677df92d737104dcb3be2bb43840c96df0abdefd
reonaldi152 <reonaldi@team.superfk.co>
2025-02-14 07:54:14 +0700

    feat: API getcart & addToCart

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,17 +3,110 @@
 namespace App\Http\Controllers;
 
 use App\Models\Cart;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use App\Helper\ResponseFormatter;
+use Illuminate\Support\Facades\Validator;
 
 class CartController extends Controller
 {
     private function getOrCreateCart()
     {
-        $cart = Cart::with(['items', 'address'])->where('user_id', auth()->user()->is)->first();
+        $cart = Cart::with(['items', 'address'])->where('user_id', auth()->user()->id)->first();
         if (is_null($cart)) {
             $cart = Cart::create([
+                'user_id' => auth()->user()->id,
+                'address_id' => optional(auth()->user()->addresses()->where('is_default', 1)->first())->id,
+                'courier' => null,
+                'courier_type' => null,
+                'courier_estimation' => null,
+                'courier_price' => 0,
+                'voucher_id' => null,
+                'voucher_value' => 0,
+                'voucher_cashback' => 0,
+                'service_fee' => 0,
+                'total' => 0,
+                'pay_with_coin' => 0,
+                'payment_method' => null,
+                'total_payment' => 0,
+            ]);
 
+            $cart->refresh();
+        }
+
+        // Calculate voucher
+        if ($cart->voucher != null) {
+            $voucher = $cart->voucher;
+            if ($voucher->voucher_type == 'discount') {
+                $cart->voucher_value = $voucher->discount_cashback_type == 'percentage' ? $cart->items->sum('total') * $voucher->discount_cashback_value / 100 : $voucher->discount_cashback_value;
+                if (!is_null($voucher->discount_cashback_max) && $cart->voucher_value > $voucher->discount_cashback_max) {
+                    $cart->voucher_value = $voucher->discount_cashback_max;
+                }
+            } elseif ($voucher->voucher_type == 'cashback') {
+                $cart->voucher_cashback = $voucher->discount_cashback_type == 'percentage' ? $cart->items->sum('total') * $voucher->discount_cashback_value / 100 : $voucher->discount_cashback_value;
+                if (!is_null($voucher->discount_cashback_max) && $cart->voucher_cashback > $voucher->discount_cashback_max) {
+                    $cart->voucher_cashback = $voucher->discount_cashback_max;
+                }
+            }
+        }
+
+        // Recalculate total
+        $cart->total = ($cart->items->sum('total')) + $cart->courier_price + $cart->service_fee - $cart->voucher_value;
+        if ($cart->total < 0) {
+            $cart->total = 0;
+        }
+        $cart->total_payment = $cart->total - $cart->pay_with_coin;
+        $cart->save();
+
+        return $cart;
+    }
+
+    public function getCart()
+    {
+        $cart = $this->getOrCreateCart();
+
+        return ResponseFormatter::success([
+            'cart' => $cart->api_response,
+            'items' => $cart->items->pluck('api_response')
+        ]);
+    }
+
+    public function addToCart()
+    {
+        $validator = Validator::make(request()->all(), [
+            'product_uuid' => 'required|exists:products,uuid',
+            'qty' => 'required|numeric|min:1',
+            'note' => 'nullable|string',
+            'vartiations' => 'nullable|array',
+            'variations.*.label' => 'required|exists:variations,name',
+            'variations.*.value' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return ResponseFormatter::error(400, $validator->errors());
+        }
+
+        $cart = $this->getOrCreateCart();
+        $product = Product::where('uuid', request()->product_uuid)->firstOrFail();
+        if ($product->stock < request()->qty) {
+            return ResponseFormatter::error(400, null, [
+                'Stock tidak cukup!'
             ]);
         }
+
+        if ($cart->items->isNotEmpty() && $cart->items->first()->product->seller_id != $product->seller_id) {
+            return ResponseFormatter::error(400, null, [
+                'Keranjang hanya boleh diisi oleh produk dari penjual yang sama!'
+            ]);
+        }
+
+        $cart->items()->create([
+            'product_id' => $product->id,
+            'variations' => request()->variations,
+            'qty' => request()->qty,
+            'note' => request()->note,
+        ]);
+
+        return $this->getCart();
     }
 }
