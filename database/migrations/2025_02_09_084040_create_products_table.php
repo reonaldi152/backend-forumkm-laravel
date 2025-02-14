d1197ab802a43c28c20a6c214f87f7156a66b9c2
reonaldi152 <reonaldi@team.superfk.co>
2025-02-14 07:59:46 +0700

    fix: migration product import

--- database/migrations/2025_02_09_084040_create_products_table.php
+++ database/migrations/2025_02_09_084040_create_products_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -13,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->uuid('uuid')->default(\DB::raw('(UUID())'));
+            $table->uuid('uuid')->default(DB::raw('(UUID())'));
             $table->unsignedBigInteger('seller_id');
             $table->string('slug');
             $table->string('name');
